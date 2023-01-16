844a5b862501856a30d418692041816a2d1ac76e
cartagoReminiscence <abjos7@protonmail.com>
2023-01-16 02:56:32 -0500

    day 10: completed. Day 11: in progress ...

--- day10_12/test_guess.py
+++ day10_12/test_guess.py
@@ -8,7 +8,8 @@ def test_get_random_number(m):
     m.return_value = 10
     assert get_random_number() == 10
 
-@patch("builtins.input", side_effect=[9, '14', 'ana', 14, 5, -10, 24, None])
+side_effect = [9, '14', 'ana', 14, 5, -10, 24, None]
+@patch("builtins.input", side_effect=side_effect)
 def test_guess(inp):
     game = Game()
     #good
@@ -22,7 +23,7 @@ def test_guess(inp):
         game.guess()
     #good
     assert game.guess() == 5
-    #out of the range
+    #out of range
     with pytest.raises(ValueError):
         game.guess()
     with pytest.raises(ValueError):
@@ -30,3 +31,51 @@ def test_guess(inp):
     #input value: empty 
     with pytest.raises(ValueError):
         game.guess()
+
+def test_validate_guess(capfd):
+    game = Game()
+    game._answer = 7
+
+    #too high or low
+    assert not game._validate_guess(6)
+    out, _ = capfd.readouterr()
+    assert out.rstrip() == '6 is too low'
+    assert not game._validate_guess(8)
+    out, _ = capfd.readouterr()
+    assert out.rstrip() == '8 is too high'
+    #good
+    assert game._validate_guess(7)
+    out, _ = capfd.readouterr()
+    assert out.rstrip() == '7 is correct!'
+
+side_effect=[1, 20, 14, 7, 10]
+@patch('builtins.input', side_effect=side_effect)
+def test_game_win(inp, capfd):
+    game = Game()
+    game._answer = 10
+
+    game()
+    assert game._win is True
+
+    out, _ = capfd.readouterr()
+    expected = ['1 is too low', '20 is too high',
+                '14 is too high', '7 is too low',
+                '10 is correct!','It took you 5 guesses'
+                ]
+
+    output = [line for line in out.split('\n') if line.rstrip()]
+    for line, exp in zip(expected, output):
+        assert line == exp
+
+side_effect=[None, 4, 5, 11, 9, 1]
+@patch('builtins.input', side_effect=side_effect)
+def test_game_lose(inp, capfd):
+    game = Game()
+    game._answer = 10
+
+    game()
+    assert game._win is False
+
+    out = capfd.readouterr()[0]
+    print("\n")
+    print(out.rstrip())
